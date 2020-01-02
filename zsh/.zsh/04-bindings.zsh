fcc706ff1d033c50bfde6948fe2d662663e002b5
rishipuri <rishipuri@live.in>
2020-01-02 13:17:21 +0000

    Fix binding for history search

--- zsh/.zsh/04-bindings.zsh
+++ zsh/.zsh/04-bindings.zsh
@@ -3,10 +3,10 @@ bindkey -v
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
 
-autoload -U up-line-or-beginning-search
+# search history on up/down keys
+autoload -U up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
-bindkey "^[[A" up-line-or-beginning-search
-
-autoload -U down-line-or-beginning-search
 zle -N down-line-or-beginning-search
-bindkey "^[[B" down-line-or-beginning-search
+
+bindkey "$key[Up]" up-line-or-beginning-search
+bindkey "$key[Down]" down-line-or-beginning-search
