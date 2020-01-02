c3a44f7eef474f5b21ec9aa6185ae4d0c619191a
rishipuri <rishipuri@live.in>
2020-01-02 15:13:11 +0000

    Amend keybindings to cycle through completion backwards

--- zsh/.zsh/04-bindings.zsh
+++ zsh/.zsh/04-bindings.zsh
@@ -3,10 +3,13 @@ bindkey -v
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
 
-# search history on up/down keys
+# search history on Up/Down keys
 autoload -U up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-bindkey "$key[Up]" up-line-or-beginning-search
-bindkey "$key[Down]" down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
+
+# move through completion backwards using Shift-Tab key
+bindkey "^[[Z" reverse-menu-complete
