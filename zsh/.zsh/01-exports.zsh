ac4a1ab1fa9b014443ff1c6d2e392916231c9e24
rishipuri <rishipuri@live.in>
2020-01-02 11:34:12 +0000

    Fix path

--- zsh/.zsh/01-exports.zsh
+++ zsh/.zsh/01-exports.zsh
@@ -1,4 +1,4 @@
-export PATH=$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
+export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH
 
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
