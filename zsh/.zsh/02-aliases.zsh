7dcb854fbe22d35a21fb40ab939df88dbbe14a11
rishipuri <rishipuri@live.in>
2020-01-02 11:52:18 +0000

    Add some aliases

--- zsh/.zsh/02-aliases.zsh
+++ zsh/.zsh/02-aliases.zsh
@@ -1,6 +1,28 @@
+alias vi='vim'
+
+alias zshrc='vi ~/.zshrc'
+alias srczshrc='source ~/.zshrc'
+
+alias bashrc='vi ~/.bashrc'
+alias srcbashrc='source ~/.bashrc'
+
 alias lsa='ls -lah'
 alias l='ls -lah'
 alias ll='ls -lh'
 alias la='ls -lAh'
 
-alias vi='vim'
+alias hgid='hg identify'
+alias hgup='hg update'
+alias hgst='hg status'
+alias hgrev='hg revert'
+alias hgi='hg incoming | more'
+alias hgim='hg incoming mainline | more'
+alias hgl='hg pull'
+alias hglm='hg pull mainline'
+alias hgo='hg outgoing'
+alias hgd='hg diff'
+alias hgsl='hg log --limit 20 --template "{node|short} \
+    | {date|isodatesec} \
+    | {pad(author|user, 10)}: {desc|strip|firstline}\n"'
+
+alias tmuxi='tmuxinator'
