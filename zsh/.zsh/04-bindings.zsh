c022fc73720cbbbbd7d4211dc4853c833bf21b9b
rishipuri <rishipuri@live.in>
2020-02-25 12:33:38 +0000

    Add some useful zsh keybindings

--- zsh/.zsh/04-bindings.zsh
+++ zsh/.zsh/04-bindings.zsh
@@ -1,7 +1,13 @@
 bindkey -v
 
 bindkey "^A" beginning-of-line
+bindkey "^[[1~" beginning-of-line
+
 bindkey "^E" end-of-line
+bindkey "^[[4~" end-of-line
+
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
 
 # search history on Up/Down keys
 autoload -U up-line-or-beginning-search down-line-or-beginning-search
@@ -9,7 +15,16 @@ zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
 bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[5~" up-line-or-beginning-search
+
 bindkey "^[[B" down-line-or-beginning-search
+bindkey "^[[6~" down-line-or-beginning-search
 
 # move through completion backwards using Shift-Tab key
 bindkey "^[[Z" reverse-menu-complete
+
+# delete backward with Backspace
+bindkey "^?" backward-delete-char
+
+# delete forward with Delete
+bindkey "^[[3~" delete-char
