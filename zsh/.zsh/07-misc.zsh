253358dadb2fccc49b23c201f3418b433e324ea5
rishipuri <rishipuri@live.in>
2022-10-04 16:42:46 +0100

    Changes for macos

--- zsh/.zsh/07-misc.zsh
+++ zsh/.zsh/07-misc.zsh
@@ -10,12 +10,17 @@ sourceif () {
     [ -f $1 ] && source $1
 }
 
+# fzf completion
+sourceif ~/.fzf.zsh
+
 # source plugins
-sourceif /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-sourceif /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+sourceif $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+sourceif $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # source nvm
-sourceif /usr/share/nvm/init-nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh"  ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 # source pyenv
 export PYENV_ROOT="$HOME/.pyenv"
