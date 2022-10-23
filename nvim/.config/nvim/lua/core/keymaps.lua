da398003802947b2228f703073ee0d959962fa3a
rishipuri <rishipuri@live.in>
2022-10-23 22:08:18 +0100

    Add config for neovim

---
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,26 @@
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+vim.g.mapleader = ' '
+
+-- buffer navigation
+map('n', '<Leader>h', ':bprevious<CR>')
+map('n', '<Leader>l', ':bnext<CR>')
+map('n', '<Leader>q', ':bdelete<CR>')
+map('n', '<Leader><Left>', ':bprevious<CR>')
+map('n', '<Leader><Right>', ':bnext<CR>')
+
+-- clear highlighting
+map('n', '\\', ':noh<CR>')
+
+-- remap esc
+map('i', 'jk', '<Esc>')
+
+-- NvimTree
+map('n', '<Leader>b', ':NvimTreeToggle<CR>')
+map('n', '<Leader>f', ':NvimTreeFindFile<CR>')
